c31e4c1100ee575ab8efc9cada5abcad9e15e83f
DOUCENE Abderrezak <99297683+pitchou15@users.noreply.github.com>
2022-12-05 19:55:18 +0100

    question 2 singelton

---
+++ src/Singleton.java
@@ -0,0 +1,16 @@
+classe  publique Singleton {
+
+	 instance Singleton statique  privée ; 
+	 singleton privé () {} 
+
+	public  static  Singleton  getInstance () {
+		if ( instance == nul ) { 
+			instance = nouveau  singleton ();
+
+		}
+
+		 instance de retour ;
+
+	}
+
+}
